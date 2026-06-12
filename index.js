8021dbd88beb87f51fb56dd8ae654a5eb3af3caf
Nkuma David <nkumadavid50@gmail.com>
2026-06-12 22:57:29 +0100

    Add my code to index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const user = {
+    name: "David",
+    age: 16,
+    isActive: false,
+    favoriteLanguage: "JavaScript"
+};
+
+console.log("Name:", user.name);
+console.log("Age:", user.age);
+console.log("Favorite Language:", user.favoriteLanguage);
